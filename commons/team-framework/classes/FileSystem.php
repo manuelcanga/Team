eb69d510a97e4a0e0b360c0e8619bab50b1eb168
Manuel Canga <dev@trasweb.net>
2016-09-05 12:31:52 +0200

    No windows, please

--- commons/team-framework/classes/FileSystem.php
+++ commons/team-framework/classes/FileSystem.php
@@ -214,10 +214,6 @@ final  class Filesystem
         if ( strlen($path) == 0 || $path[0] == '.' )
             return false;
 
-        // Windows allows absolute paths like this.
-        if ( preg_match('#^[a-zA-Z]:\\\\#', $path) )
-            return true;
-
         // A path starting with / or \ is absolute; anything else is relative.
         return ( $path[0] == '/' || $path[0] == '\\' );
     }
@@ -243,20 +239,12 @@ final  class Filesystem
     /**
      * Normalize a filesystem path.
      *
-     * On windows systems, replaces backslashes with forward slashes
-     * and forces upper-case drive letters.
-     * Allows for two leading slashes for Windows network shares, but
-     * ensures that all other duplicate slashes are reduced to a single.
-     *
      * @param string $path Path to normalize.
      * @return string Normalized path.
      */
     function normalizePath( $path ) {
         $path = str_replace( '\\', '/', $path );
-        $path = preg_replace( '|(?<=.)/+|', '/', $path );
-        if ( ':' === substr( $path, 1, 1 ) ) {
-            $path = ucfirst( $path );
-        }
+
         return $path;
     }
 
