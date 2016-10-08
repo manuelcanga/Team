723732ef16b8c0c02e43f30a483fb66003f79741
Manuel Canga <dev@trasweb.net>
2016-10-08 15:34:12 +0200

    Adding http status functions

--- commons/team-framework/classes/Http.php
+++ commons/team-framework/classes/Http.php
@@ -167,4 +167,108 @@ class Http
         exit();
     }
 
+
+
+    /**
+     * Retrieve the description for the HTTP status.
+     *
+     * @param int $code HTTP status code.
+     * @return string Empty string if not found, or description if found.
+     */
+    function getStatusHeaderDesc( $code ) {
+        static $code2header_desc;
+
+        $code = absint( $code );
+
+        if ( !isset( $code2header_desc ) ) {
+            $code2header_desc = array(
+                100 => 'Continue',
+                101 => 'Switching Protocols',
+                102 => 'Processing',
+
+                200 => 'OK',
+                201 => 'Created',
+                202 => 'Accepted',
+                203 => 'Non-Authoritative Information',
+                204 => 'No Content',
+                205 => 'Reset Content',
+                206 => 'Partial Content',
+                207 => 'Multi-Status',
+                226 => 'IM Used',
+
+                300 => 'Multiple Choices',
+                301 => 'Moved Permanently',
+                302 => 'Found',
+                303 => 'See Other',
+                304 => 'Not Modified',
+                305 => 'Use Proxy',
+                306 => 'Reserved',
+                307 => 'Temporary Redirect',
+                308 => 'Permanent Redirect',
+
+                400 => 'Bad Request',
+                401 => 'Unauthorized',
+                402 => 'Payment Required',
+                403 => 'Forbidden',
+                404 => 'Not Found',
+                405 => 'Method Not Allowed',
+                406 => 'Not Acceptable',
+                407 => 'Proxy Authentication Required',
+                408 => 'Request Timeout',
+                409 => 'Conflict',
+                410 => 'Gone',
+                411 => 'Length Required',
+                412 => 'Precondition Failed',
+                413 => 'Request Entity Too Large',
+                414 => 'Request-URI Too Long',
+                415 => 'Unsupported Media Type',
+                416 => 'Requested Range Not Satisfiable',
+                417 => 'Expectation Failed',
+                418 => 'I\'m a teapot',
+                421 => 'Misdirected Request',
+                422 => 'Unprocessable Entity',
+                423 => 'Locked',
+                424 => 'Failed Dependency',
+                426 => 'Upgrade Required',
+                428 => 'Precondition Required',
+                429 => 'Too Many Requests',
+                431 => 'Request Header Fields Too Large',
+                451 => 'Unavailable For Legal Reasons',
+
+                500 => 'Internal Server Error',
+                501 => 'Not Implemented',
+                502 => 'Bad Gateway',
+                503 => 'Service Unavailable',
+                504 => 'Gateway Timeout',
+                505 => 'HTTP Version Not Supported',
+                506 => 'Variant Also Negotiates',
+                507 => 'Insufficient Storage',
+                510 => 'Not Extended',
+                511 => 'Network Authentication Required',
+            );
+        }
+
+        return $code2header_desc[$code]?? '';
+    }
+
+
+    /**
+     * Set HTTP status header.
+     *
+     * @param int    $code        HTTP status code.
+     * @param string $description Optional. A custom description for the HTTP status.
+     */
+    function sendStatusHeader( int $code, $description = '' ) {
+        if ( ! $description ) {
+           http_response_code($code);
+            return ;
+        }
+
+        $protocol = \team\Context::get('PROTOCOL');
+        $status_header = "$protocol $code $description";
+
+        $status_header = \team\Filter::apply('\team\http\status_header', $status_header, $code, $description, $protocol);
+
+        @header( $status_header, true, $code );
+    }
 }
