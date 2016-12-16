609182cca84703c02d4b2bc9ae6075ae494c9328
Manuel Canga <dev@trasweb.net>
2016-12-16 21:56:43 +0100

    Eliminada función is_ssl porque ya se hace en el config por defecto de Url.conf.php

--- commons/team-framework/classes/Enviroment.php
+++ commons/team-framework/classes/Enviroment.php
@@ -71,22 +71,4 @@ class Enviroment
     }
 
 
-    /**
-     * Determine if SSL is used.
-     *
-     * @since 2.6.0
-     *
-     * @return bool True if SSL, false if not used.
-     */
-    function is_ssl() {
-        if ( isset($_SERVER['HTTPS']) ) {
-            if ( 'on' == strtolower($_SERVER['HTTPS']) )
-                return true;
-            if ( '1' == $_SERVER['HTTPS'] )
-                return true;
-        } elseif ( isset($_SERVER['SERVER_PORT']) && ( '443' == $_SERVER['SERVER_PORT'] ) ) {
-            return true;
-        }
-        return false;
-    }
 }
