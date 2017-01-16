02d141202aa09c4fb3e9dc3d9a8040000b53a9fc
Manuel Canga <dev@trasweb.net>
2017-01-16 18:27:05 +0100

    Hacemos un md5 del compile id y eso lo usamos como compile id. Así evitamos que compile id largos creen desbordamiento en nombre de archivos temporales smarty

--- commons/team-framework/classes/data/htmlengines/TemplateEngine.php
+++ commons/team-framework/classes/data/htmlengines/TemplateEngine.php
@@ -251,7 +251,7 @@ class TemplateEngine implements \team\interfaces\data\HtmlEngine{
 
 		 $_engine->compile_check = !$view_cache;
 		 $_engine->caching = $view_cache;
-		 $compile_id = $package.$component.$response.$_template.\team\Config::get('URL').\team\Context::get('COMPILE_ID');
+		 $compile_id = md5($package.$component.$response.$_template.\team\Config::get('URL').\team\Context::get('COMPILE_ID'));
 		 $_engine->compile_id =  \team\Filter::apply('\team\smarty\compile_id',$compile_id );
 
 		
