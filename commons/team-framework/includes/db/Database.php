b79b35593efb0ba285b0ccd6852d13ba53d7453e
Manuel Canga <dev@trasweb.net>
2017-03-04 18:56:11 +0100

    Added a new method for static methods

--- commons/team-framework/includes/db/Database.php
+++ commons/team-framework/includes/db/Database.php
@@ -8,9 +8,11 @@ trait Database {
 		return \team\DB::get($name_new_conection, get_class($this));
     }
 
-    protected function newQuery($values = null, array $sentences = [],   $name_new_conection = null, &$database = null) {
+    protected function newQuery($values = null, array $sentences = [],   $name_new_conection = null) {
         return new Query($values, $this->getDatabase($name_new_conection), $sentences);
     }
     
-
+    static function getNewQuery($values = null, array $sentences = [],   $name_new_conection = null) {
+        return new Query($values, \team\DB::get($name_new_conection, static::class), $sentences);
+    }
 }
