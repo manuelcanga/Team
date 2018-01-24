205dffc75e99b15ed995bebfb2a804b8cf9e2a8d
Manuel Canga <dev@trasweb.net>
2018-01-24 17:48:50 +0100

    Added constructors and sanitiziers to config vars

--- vendor/Team/Config.php
+++ vendor/Team/Config.php
@@ -41,13 +41,96 @@ abstract class Config{
 
     protected static $vars = [];
     protected static $modifiers = [];
+    protected static $sanitizers = [];
+    protected static $constructors = [];
+
 
     public static function setup() {
         \Team::event('\team\setup', self::$vars);
     }
 
     public static function get(string $var_name, $default = null, $place = null) {
-        return self::applyModifiers($var_name, self::$vars[$var_name]?? $default, $place );
+        return self::applyModifiers($var_name, self::$vars[$var_name]?? self::applyConstructors($var_name, $default), $place );
+    }
+
+    public static function set($var, $value = null) {
+        $old_value = null;
+
+        if(is_array($var)) {
+            self::$vars =  $var + self::$vars;
+        }else if(is_string($var)){
+            $old_value = self::$vars[$var]?? $old_value;
+
+            self::$vars[$var] = static::applySanitizers($var, $value);
+        }
+
+        return $old_value;
+    }
+
+    public static function addSanitizer($config_var, $function, int $order = 50){
+
+        self::$sanitizers[$config_var] = self::$sanitizers[$config_var]?? [];
+
+        //Vamos buscando un hueco libre para el sanitizer a partir del orden que pidió
+        for($max_order = 100; isset(self::$sanitizers[$config_var][$order]) && $order < $max_order; $order++);
+
+        //Lo almacemanos todo para luego poder usarlo
+        self::$sanitizers[$config_var][$order] =  $function;
+
+        return false;
+    }
+
+
+    protected static function applySanitizers($config_var, $value) {
+        if(!isset(self::$sanitizers[$config_var])  ) return $value;
+
+        $sanitizers =& self::$sanitizers[$config_var];
+
+        ksort($sanitizers);
+
+        foreach($sanitizers as $sanitizer) {
+            if(!is_callable($config_var,  $syntax_only = true)) {
+                \Team\Debug::me('You are adding a sanitizier to ' . $config_var . ' which isn\'t a callback');
+                return false;
+            }else {
+                $value = $sanitizer($value);
+            }
+        }
+        return $value;
+    }
+
+
+    public static function addConstructor($config_var, $function, int $order = 50){
+
+        self::$constructors[$config_var] = self::$constructors[$config_var]?? [];
+
+        //Vamos buscando un hueco libre para el constructor a partir del orden que pidió
+        for($max_order = 100; isset(self::$constructors[$config_var][$order]) && $order < $max_order; $order++);
+
+        //Lo almacemanos todo para luego poder usarlo
+        self::$constructors[$config_var][$order] =  $function;
+
+        return false;
+    }
+
+
+    protected static function applyConstructors($config_var, $default) {
+        if(!isset(self::$constructors[$config_var])  ) return $default;
+
+        $constructors =& self::$constructors[$config_var];
+
+        ksort($constructors);
+
+        $value = $default;
+        foreach($constructors as $constructor) {
+            if(!is_callable($config_var,  $syntax_only = true)) {
+                \Team\Debug::me('You are adding a constructor to ' . $config_var . ' which isn\'t a callback');
+                return false;
+            }else {
+                $value = $constructor($default);
+            }
+        }
+        return $value;
     }
 
     public static function addModifier($config_var, $function, int $order = 50){
@@ -62,6 +145,7 @@ abstract class Config{
 
         return false;
     }
+
 
     protected static function applyModifiers($config_var, $value, $place) {
         if(!isset(self::$modifiers[$config_var])  ) return $value;
@@ -80,4 +164,6 @@ abstract class Config{
         }
         return $value;
     }
+
+
 }
